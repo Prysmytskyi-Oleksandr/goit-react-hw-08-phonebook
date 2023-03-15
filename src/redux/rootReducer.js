b1a3fa8caf8ce36f5b2881e5490bd513addae7c4
Prysmytskyi-Oleksandr <san2003@ukr.net>
2023-03-15 22:20:01 +0200

    222

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,21 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import contactsReducer from './contacts/contactsSlice';
 import filterReducer from './filter/filterSlice';
 import authReducer from './auth/authSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['token'],
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
 const rootReducer = combineReducers({
-  auth: authReducer,
+  auth: persistedAuthReducer,
   contacts: contactsReducer,
   filter: filterReducer,
 });
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
+import {
+  persistStore,
+  // FLUSH,
+  // REHYDRATE,
+  // PAUSE,
+  // PERSIST,
+  // PURGE,
+  // REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
   reducer: rootReducer,
 });
+export const persistor = persistStore(store);
